3c1598ca67818fb343ef67d93b43733a3764ee25
shadow-001sama <rohankadam2208@gmail.com>
2026-02-22 14:13:34 +0530

    Added coupon discount functionality
    Signed-off-by: shadow-001sama <rohankadam2208@gmail.com>

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ total += item.price;
 document.getElementById("cartItems").innerHTML = output;
 document.getElementById("total").innerHTML = "Total: ₹" + total;
 }
+let discountAmount = 0;
+
+function applyCoupon(){
+let code = document.getElementById("coupon").value;
+let total = cart.reduce((sum,item)=>sum+item.price,0);
+
+if(code === "STRYKO10"){
+discountAmount = total * 0.10;
+document.getElementById("discount").innerHTML = 
+"Discount Applied: ₹" + discountAmount.toFixed(0);
+document.getElementById("total").innerHTML = 
+"Final Total: ₹" + (total - discountAmount).toFixed(0);
+}else{
+document.getElementById("discount").innerHTML = 
+"Invalid Coupon Code";
+}
+                        }
