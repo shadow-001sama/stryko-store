1b2bae57ec2e924f0c999d7eda261d662e0c8eb0
shadow-001sama <rohankadam2208@gmail.com>
2026-02-22 14:18:08 +0530

    Added wishlist functionality
    Signed-off-by: shadow-001sama <rohankadam2208@gmail.com>

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ document.getElementById("discount").innerHTML =
 "Invalid Coupon Code";
 }
                         }
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+function addWishlist(name){
+wishlist.push(name);
+localStorage.setItem("wishlist", JSON.stringify(wishlist));
+alert(name + " added to wishlist ❤️");
+}
